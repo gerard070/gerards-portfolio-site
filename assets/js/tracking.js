ca3dd8374b6fe9cdda8be1b9dcf09dabef0691d6
Gerard Connolly <geconn117@gmail.com>
2025-05-03 14:04:32 -0400

    adding google analytics tracking.js and script tags to pages

---
+++ assets/js/tracking.js
@@ -0,0 +1,11 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-81XY7HRZLR';
+    document.head.appendChild(script);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-81XY7HRZLR');
+  })();
